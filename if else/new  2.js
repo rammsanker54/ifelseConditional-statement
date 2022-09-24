f8359309b2d24a00ad45dea4da4fd08f5b10a8a3
rammsanker <109334253+rammsanker54@users.noreply.github.com>
2022-09-24 11:47:10 +0530

    Add files via upload

---
+++ if else/new  2.js
@@ -0,0 +1,148 @@
+if (a>b && a>c){
+if (b>c){
+console.log(a,b,c);}
+else{console.log(a,c,b);}
+}
+else if (b>a && b>c){
+if (a>c){
+console.log(b,a,c);}
+else{
+console,log(b,c,a);}}
+else if (c>a && c>b){
+if (a>b){
+console.log(c,a,b);}
+else{
+   console.log(c,b,a);}}
+
+
+ let a = 36;
+ if (a%6==0){
+ console.log(a);}
+ else {
+ console.log ("not 6 divisible");}
+ let a = 
+ for (i=1;i<10;i++)
+  {console.log(i*a);}
+  
+  let a = 30;
+  let b = 40;
+  for (i=0;i<=100;i++){
+   if (a-b==9){
+   console.log("9divisible")}
+   else{
+   console.log("not 9 divi")
+   
+   
+   let a = 30;
+   let b = 40;
+   let c = 50;
+   let d = 70;
+    if (a>b && a>c && a>d){
+	if (b>c && b>d){
+	if (c>d){console.log(a,b,c,d);}
+	else{console.log(a,b,d,c);}
+	if (c>b && c>d){
+	if (b>d){
+	console.log(a,c,b,d);}
+	else{ console.log(c,a,b,d);}
+	else if (d>b && d>c){
+	if(b>c) {
+	console.log(a,d,b,c);}
+	else{console.log(a,d,c,b)}
+	}
+	else if (b>a && b>d && b>c){
+	if (a>c && a>d){
+	if (c>d) {
+	console.log(b,a,c,d);}
+	else{console.log(b,a,d,c);}}
+	else if (c>a && c>d){
+     if(a>d) {
+console.log(c,b,a,d);}	
+else{console.log(c,b,d,a);}
+//
+
+
+
+
+
+
+
+let a = 20;
+let b = 30;
+let c = 90;
+	if (a>b && a>c){
+	if (b>c){
+	console.log(a,b,c);
+	}
+	else{
+	console.log(a,c,b);
+	}}
+	else if (b>a && b>c){
+	if (a>c){
+	console.log(b,a,c);}
+	else{
+	console.log(b,c,a);}}
+	else if (c>a && c>b){
+	if (a>b){
+	console.log(c,a,b);}
+	else{
+	console.log(c,b,a);}}
+
+let a = 2020;
+if (a%4==0)
+{
+console.log("its a leap year");}
+else{
+console.log("not a  leap year");}
+
+let a =  ;
+let b = 
+if (a%2==0){
+console.log("can't add");}
+else {console.log(a+a);}
+
+let a = ;
+for (i=1;i<a;i++)
+{console.log(a+a);}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+  
+  
+  
+  
